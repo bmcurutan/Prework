22f9be85be7307ffca2e19323580bbff3d9b6cb9
Bianca Curutan <bmcurutan@gmail.com>
2017-02-27 22:55:34 -0800

    zig zag tree

--- scratchpad/src/ZigZagTree.java
+++ scratchpad/src/ZigZagTree.java
@@ -1,5 +1,3 @@
-package NotDone;
-
 import java.util.*;
 
 public class ZigZagTree {
@@ -18,39 +16,56 @@ public class ZigZagTree {
     // Print to the right, then left
     public static void printZigzag(TreeNode t) {
         if (t == null) {
+            System.out.println();
             return;
         }
 
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(t);
-        Stack<TreeNode> s = new Stack<>();
-        while (!q.isEmpty()) {
-            TreeNode pop = q.poll();
-            System.out.print(pop.val + " ");
-            if (pop.left != null) {
-                q.add(pop.left);
+        Map<TreeNode, Boolean> map = new LinkedHashMap <>(); // true go to the right, false go to the left
+        map.put(t, true);
+        String temp = "";
+        while (!map.isEmpty()) {
+            Map.Entry<TreeNode, Boolean> entry = map.entrySet().iterator().next();
+            map.remove(entry.getKey());
+
+            if (entry.getValue()) {
+                System.out.print(temp);
+                temp = "";
+                System.out.print(entry.getKey().val + " ");
+            } else {
+                temp = entry.getKey().val + " " + temp;
+                //System.out.println(temp);
             }
-            if (pop.right != null) {
-                q.add(pop.right);
+
+            if (entry.getKey().left != null) {
+                map.put(entry.getKey().left, !entry.getValue());
             }
+            if (entry.getKey().right != null) {
+                map.put(entry.getKey().right, !entry.getValue());
+            }
+
+            /*for(Map.Entry<TreeNode, Boolean> e : map.entrySet()) {
+                System.out.print(e.getKey().val + " * ");
+            }
+            System.out.println();*/
         }
 
+        System.out.print(temp);
         System.out.println();
     }
 
     public static void main(String[] args) {
         TreeNode test1 = null;
-        System.out.println("Test 1");
+        System.out.print("Test 1: ");
         printZigzag(test1);
 
         TreeNode test2 = new TreeNode(1);
-        System.out.println("Test 2");
+        System.out.print("Test 2: ");
         printZigzag(test2); // 1
 
         TreeNode test3 = new TreeNode(1);
         test3.left = new TreeNode(2);
         test3.right = new TreeNode(3);
-        System.out.println("Test 3");
+        System.out.print("Test 3: ");
         printZigzag(test3); // 1 3 2
 
         TreeNode test4 = new TreeNode(1);
@@ -63,8 +78,7 @@ public class ZigZagTree {
         test4.left.right.right = new TreeNode(11);
         test4.right = new TreeNode(3);
         test4.right.left = new TreeNode(6);
-        test4.right.right = new TreeNode(7);
-        System.out.println("Test 4");
-        printZigzag(test4); // 1 3 2 4 5 6 7 11 10 9 8
+        System.out.print("Test 4: ");
+        printZigzag(test4); // 1 3 2 4 5 6 11 10 9 8
     }
 }
